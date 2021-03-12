cd4bf8932b8b104069e43ac298f1244a999bdb43
Bruno Michetti <brunomichetti@rootstrap.com>
2021-03-12 10:39:18 -0300

    First init

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+GENDER_SELECTION = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('NS', 'Not Specified'),
+]
+
+
+class CustomUser(AbstractUser):
+    # We don't need to define the email attribute because is inherited from AbstractUser
+    gender = models.CharField(max_length=20, choices=GENDER_SELECTION)
+    phone_number = models.CharField(max_length=30)
