f36f6bb9fe701c1df519b0f1efff2713993d6deb
dohaelsawy <dohaelsawy18@gmail.com>
2024-04-25 13:12:09 +0200

    user auth system and classification function

--- users/models.py
+++ users/models.py
@@ -7,11 +7,20 @@ from django.contrib.auth.models import AbstractUser
 GENDER_SELECTION = [
     ('M', 'Male'),
     ('F', 'Female'),
-    ('NS', 'Not Specified'),
 ]
 
 
+
+def upload_to(instance, filename):
+    return 'user_image/{filename}'.format(filename=filename)
+
 class CustomUser(AbstractUser):
-    # We don't need to define the email attribute because is inherited from AbstractUser
     gender = models.CharField(max_length=20, choices=GENDER_SELECTION)
     phone_number = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    date_of_birth = models.DateField(null=True)
+    is_premium = models.BooleanField(default=False)
+    age = models.IntegerField(blank=True, null=True)
+    user_image = models.ImageField(upload_to=upload_to, blank=True, null=True)
+
+
--- users/serializers.py
+++ users/serializers.py
@@ -2,20 +2,39 @@
 from django.db import transaction
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
-
+from datetime import date, datetime
 from users.models import GENDER_SELECTION, CustomUser
+
+def calculateAge(date_of_birth):
+    date_of_birth = date.fromisoformat(date_of_birth)
+    today = date.today()
+    years_difference = today.year - date_of_birth.year
+    is_before_birthday = (today.month, today.day) < (date_of_birth.month, date_of_birth.day)
+    age = years_difference - int(is_before_birthday)
+    return age
 
 
 class CustomRegisterSerializer(RegisterSerializer):
+    first_name = serializers.CharField(max_length=30, required=True)
+    last_name = serializers.CharField(max_length=30, required=True)
     gender = serializers.ChoiceField(choices=GENDER_SELECTION)
     phone_number = serializers.CharField(max_length=30)
+    date_of_birth = serializers.DateField(required=False)
+    profile_picture = serializers.ImageField(required=False)
 
-    # Define transaction.atomic to rollback the save operation in case of error
+
+
     @transaction.atomic
     def save(self, request):
+        """  """ 
         user = super().save(request)
         user.gender = self.data.get('gender')
         user.phone_number = self.data.get('phone_number')
+        user.first_name = self.data.get('first_name')
+        user.last_name = self.data.get('last_name')
+        user.date_of_birth = self.data.get('date_of_birth')
+        user.age = calculateAge(user.date_of_birth)
+        user.user_image = self.validated_data.get('profile_picture', None)
         user.save()
         return user
 
@@ -26,8 +45,15 @@ class CustomUserDetailsSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = (
             'pk',
+            'first_name',
+            'last_name',
             'email',
             'phone_number',
             'gender',
+            'date_of_birth',
+            'is_premium',
+            'is_active',
+            'age',
+            'user_image'
         )
         read_only_fields = ('pk', 'email', 'phone_number',)
